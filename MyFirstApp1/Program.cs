74056374fed3b1ade6ee92007e349230e8cfc1a6
Cristina <cristinamoraru7@yahoo.com>
2020-09-21 18:32:33 +0300

    first commit

---
+++ MyFirstApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyFirstApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Primary operators
+
+               //Member access operators .             
+
+               //Array indexer []
+            int[] numbers = { 1, 2, 3 };
+           // Console.WriteLine( numbers[2] );
+
+
+            //Unary operators
+            int number = 5;
+            int x = number++;
+            Console.WriteLine(x);
+            Console.WriteLine(number);
+            Console.ReadLine();
+
+
+            //Relational operators < = >
+
+            //Conditional operators AND && , OR ||
+
+
+
+        }
+    }
+}
